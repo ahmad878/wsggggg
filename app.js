07c209fde71f8157c1672a0cdab41062eb639b9b
ahmad878 <ahmadalja108@gmail.com>
2026-01-01 20:05:31 -0500

    Initial commit

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
-const port = 3000;
 
-app.post('/', function(req,res){
+const port = process.env.PORT || 3000;
+
+app.use(express.json()); // needed for req.body
+
+app.post('/', (req, res) => {
     console.log(req.body);
-})
+    res.send('OK');
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
